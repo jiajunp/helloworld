9d79786237792d7469e5c3812ae68ddb9f231ddf
jiajunp <jiajunp@163.com>
2019-07-01 22:29:47 +0800

    commit

---
+++ hdfs/src/test/java/com/jack/Test.java
@@ -0,0 +1,12 @@
+package com.jack;
+
+/**
+ * @Author: jiajunp
+ * @Date: 2019/7/1 22:28
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
